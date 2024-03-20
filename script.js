01f3bbd982f367130ef78baef940db8b7b6391a9
PetrosyanVaghinak <108834302+PetrosyanVaghinak@users.noreply.github.com>
2024-03-20 17:32:01 +0400

    change checked

--- script.js
+++ script.js
@@ -14,29 +14,30 @@ function addTask() {
     li.appendChild(span);
   }
   inputBox.value = "";
-  saveData()
+  saveData();
 }
 
-listContainer.addEventListener("click",function (e) {
-    
+listContainer.addEventListener(
+  "click",
+  function (e) {
     if (e.target.tagName === "LI") {
       e.target.classList.toggle("checked");
-      
-      saveData()
+
+      saveData();
     } else if (e.target.tagName === "SPAN") {
       e.target.parentElement.remove();
-      console.log(e.target.parentElement);
-      saveData()
+
+      saveData();
     }
   },
   false
 );
 
 function saveData() {
-    localStorage.setItem("data", listContainer.innerHTML)
+  localStorage.setItem("data", listContainer.innerHTML);
 }
 
 function showTask() {
-    listContainer.innerHTML =localStorage.getItem("data")
+  listContainer.innerHTML = localStorage.getItem("data");
 }
-showTask()
+showTask();
